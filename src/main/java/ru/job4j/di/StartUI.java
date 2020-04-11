e79ce98e21c7631e1e45a2385b9eb64d98078e72
hedg-r52 <hedg.r52@gmail.com>
2020-04-11 18:25:50 +0300

    Simple DI implementation - added dependence into StartUI class on ConsoleInput class - initialize StartUI using Context

--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -2,9 +2,11 @@ package ru.job4j.di;
 
 public class StartUI {
     private Store store;
+    private ConsoleInput consoleInput;
 
-    public StartUI(Store store) {
+    public StartUI(Store store, ConsoleInput consoleInput) {
         this.store = store;
+        this.consoleInput = consoleInput;
     }
 
     public void add(String value) {
